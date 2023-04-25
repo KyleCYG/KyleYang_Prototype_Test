ad39e484b32851da93cd6bedd4f4a63fb77efd1b
Ximing Yang <cly7100@gmail.com>
2023-04-26 02:49:03 +0800

    Update prototype.js

--- static/js/prototype.js
+++ static/js/prototype.js
@@ -1,5 +1,8 @@
 
-    // const card = document.querySelector('.card');
+    /* 检测mobile/PC */
+    function isMobile() { 
+        return ('ontouchstart' in document.documentElement); 
+    }
 
     /* 获取cards数组 */
     const card = document.getElementsByClassName('card');
@@ -9,64 +12,78 @@
     /* 打印cards数组 */
     // console.log(card);
 
-
-    /* 遍历cards, 添加监听 */
-    for (let i = 0; i < card.length; i++) {
-        const cardy = card[i];
-        var rect = 0;
-
-        /* 添加mousemove监听 */
-        cardy.addEventListener('mousemove', e => {
-            rect = cardy.getBoundingClientRect();
-            x = e.clientX - rect.left;
-            y = e.clientY - rect.top;
+    /* 判断mobile/PC */
+    var isMobile = isMobile();
+    // console.log(isMobile);
     
-            /* 打印鼠标位置 */
-            // console.log('------------');
-            // console.log('x = ' + x);
-            // console.log('y = ' + y);
-            // console.log('------------');
-            
-            /* 为card添加渐变spotlight */
-            cardy.style.setProperty("background", "radial-gradient(180px 180px at " + x + "px " + y + "px , rgba(255, 230, 214, 0.2), rgba(255, 255, 255, 0.03))");
-    
-        });
+    if (isMobile) {
+        
+        /* 遍历cards, 添加监听 */
+        for (let i = 0; i < card.length; i++) {
+            const cardy = card[i];
+            var rect = 0;
 
-        /* 添加touchmove监听 */
-        cardy.addEventListener('touchmove', e => {
+            /* 添加touchmove监听 */
+            cardy.addEventListener('touchmove', e => {
 
-            rect = cardy.getBoundingClientRect();
+                // 得到相对坐标
+                rect = cardy.getBoundingClientRect();
 
-            e.preventDefault();
-            // console.log(rect);
+                /* 防止滚动 */
+                e.preventDefault();
 
+                /* 遍历每个touch点 */
+                for (let i = 0; i < e.touches.length; i++) {
 
-            for (let i = 0; i < e.touches.length; i++) {
-                // console.log(e.touches[i].clientX);
-                x = e.touches[i].clientX - rect.left;
-                y = e.touches[i].clientY - rect.top;
+                    x = e.touches[i].clientX - rect.left;
+                    y = e.touches[i].clientY - rect.top;
+                    
+                    /* 为card添加渐变spotlight */
+                    cardy.style.setProperty("background", "radial-gradient(180px 180px at " + x + "px " + y + "px , rgba(255, 230, 214, 0.2), rgba(255, 255, 255, 0.03))");
+
+                }
+            });
+
+            /* 添加touchend监听 */
+            cardy.addEventListener('touchend', e => {
+
+                /* 移除spotlight */
+                cardy.style.removeProperty("background", "radial-gradient(180px 180px at " + x + "px " + y + "px , rgba(255, 230, 214, 0.2), rgba(255, 255, 255, 0.03))");
+        
+            });
+        }
+
+    }else{
+
+        /* 遍历cards, 添加mouse监听 */
+        for (let i = 0; i < card.length; i++) {
+            const cardy = card[i];
+            var rect = 0;
+
+            /* 添加mousemove监听 */
+            cardy.addEventListener('mousemove', e => {
+                rect = cardy.getBoundingClientRect();
+                x = e.clientX - rect.left;
+                y = e.clientY - rect.top;
+        
+                /* 打印鼠标位置 */
+                // console.log('------------');
+                // console.log('x = ' + x);
+                // console.log('y = ' + y);
+                // console.log('------------');
                 
                 /* 为card添加渐变spotlight */
                 cardy.style.setProperty("background", "radial-gradient(180px 180px at " + x + "px " + y + "px , rgba(255, 230, 214, 0.2), rgba(255, 255, 255, 0.03))");
+        
+            });
 
-            }
+            /* 添加mouseout监听 */
+            cardy.addEventListener('mouseout', e => {
+        
+                /* 移除spotlight */
+                cardy.style.removeProperty("background", "radial-gradient(180px 180px at " + x + "px " + y + "px , rgba(255, 230, 214, 0.2), rgba(255, 255, 255, 0.03))");
+        
+            });
+        }
 
-  
-        });
-
-        /* 添加mouseout监听 */
-        cardy.addEventListener('mouseout', e => {
-    
-            /* 移除spotlight */
-            cardy.style.removeProperty("background", "radial-gradient(180px 180px at " + x + "px " + y + "px , rgba(255, 230, 214, 0.2), rgba(255, 255, 255, 0.03))");
-    
-        });
-
-        /* 添加touchend监听 */
-        cardy.addEventListener('touchend', e => {
-
-            /* 移除spotlight */
-            cardy.style.removeProperty("background", "radial-gradient(180px 180px at " + x + "px " + y + "px , rgba(255, 230, 214, 0.2), rgba(255, 255, 255, 0.03))");
-    
-        });
     }
