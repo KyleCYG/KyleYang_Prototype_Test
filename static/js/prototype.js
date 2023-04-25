327b611244b8707e099514bd8a9a5c0f41b428ee
Ximing Yang <cly7100@gmail.com>
2023-04-25 22:03:09 +0800

    Add files via upload

---
+++ static/js/prototype.js
@@ -0,0 +1,42 @@
+
+    // const card = document.querySelector('.card');
+
+    /* 获取cards数组 */
+    const card = document.getElementsByClassName('card');
+    var x = null;
+    var y = null;
+
+    /* 打印cards数组 */
+    // console.log(card);
+    // console.log(rect);
+
+    /* 遍历cards, 添加监听 */
+    for (let i = 0; i < card.length; i++) {
+        const cardy = card[i];
+        var rect = 0;
+
+        /* 添加mousemove监听 */
+        cardy.addEventListener('mousemove', e => {
+            rect = cardy.getBoundingClientRect();
+            x = e.clientX - rect.left;
+            y = e.clientY - rect.top;
+    
+            /* 打印鼠标位置 */
+            // console.log('------------');
+            // console.log('x = ' + x);
+            // console.log('y = ' + y);
+            // console.log('------------');
+            
+            /* 为card添加渐变spotlight */
+            cardy.style.setProperty("background", "radial-gradient(180px 180px at " + x + "px " + y + "px , rgba(255, 230, 214, 0.2), rgba(255, 255, 255, 0.03))");
+    
+        });
+
+        /* 添加mouseout监听 */
+        cardy.addEventListener('mouseout', e => {
+    
+            /* 移除spotlight */
+            cardy.style.removeProperty("background", "radial-gradient(180px 180px at " + x + "px " + y + "px , rgba(255, 230, 214, 0.2), rgba(255, 255, 255, 0.03))");
+    
+        });
+    }
